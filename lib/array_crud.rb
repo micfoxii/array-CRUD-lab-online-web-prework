9da15ad09d0ea71da3704732b225ad859e5976b4
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-14 22:39:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
   
 def add_element_to_start_of_array(array, element)
-  front_array = []
-  front_array.unshift "wow"
+  []
+  .unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
